27640aee2cd437c73b8444da3e55806ce49933ba
ouzhx <ouzhx@lt51.cn>
2017-04-26 18:23:26 +0800

    motan rpc 使用zookeeper 作爲注冊中心通過

--- service/src/main/java/com/ouzhx/service/MotanServerRun.java
+++ service/src/main/java/com/ouzhx/service/MotanServerRun.java
@@ -1,14 +1,17 @@
 package com.ouzhx.service;
 
+import com.weibo.api.motan.common.MotanConstants;
+import com.weibo.api.motan.util.MotanSwitcherUtil;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * Created by Administrator on 2017/4/15.
  */
-public class Test {
+public class MotanServerRun {
   public static void main(String[] args) {
     AnnotationConfigApplicationContext context =
         new AnnotationConfigApplicationContext(AppConfig.class);
+    MotanSwitcherUtil.setSwitcherValue(MotanConstants.REGISTRY_HEARTBEAT_SWITCHER, true);
     System.out.println("motan server start!...");
   }
 }
